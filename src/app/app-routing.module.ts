8fd5b42f18177a5ecc3b2c4ea37098a5fba82a42
Brofessorr <76777285+Brofessorr@users.noreply.github.com>
2021-01-16 12:08:17 +0100

    F/e changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
+import { ResultsComponent } from './components/results/results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: FormComponent
+
+  },
+  {
+    path: 'results', component: ResultsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -9,7 +10,7 @@ export class FormComponent implements OnInit {
 
   searchQuery:string="";
 
-  constructor() { }
+  constructor(){ }
 
   ngOnInit(): void {
 
